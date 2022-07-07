4c14c7e1594266db164ec436e830a1ced2a89fb5
humaid123 <huagowun@gmail.com>
2022-07-07 15:01:09 -0300

    Error control with sampling experiments

---
+++ defect control/error control/rk4_with_hb4_vs_hb6_errorSampling/test_rk_constant_alpha.py
@@ -0,0 +1,144 @@
+from rk4 import rk_error_control_static_alpha_beta
+from math import exp, floor
+import matplotlib.pyplot as plt
+
+def create_t_eval(start, end, num_points = 100):
+    res = [start]
+    h = (end - start)/ (num_points - 1)
+
+    for _ in range(num_points - 1):
+        res.append(
+            res[-1] + h
+        )
+    return res
+
+t_span_3 = [0, 10]
+y0_3 = [1]
+
+def model3(t, y):
+    return [(1/4)*y*(1 - y/20)]
+
+def solution3(t):
+    return [20 / ( 1 + 19 * exp(-x/4) ) for x in t]
+
+t_span_2 = [0, 10]
+y0_2 = [1]
+
+def model2(t, y):
+    return [-2*t*y**2]
+
+def solution2(t):
+    return [1/(1+x**2) for x in t]
+
+
+def experiment(model, solution, y0, t_span):
+    t_eval = create_t_eval(t_span[0], t_span[1])
+    actual_solutions = solution(t_eval)
+
+    tols = [1e-8, 1e-9, 1e-10]
+    for tol in tols:
+        res, sol, first_deriv, error_samplings = rk_error_control_static_alpha_beta(model, t_span, y0[0], tol, solution)
+        computed_solutions = [sol(x) for x in t_eval]
+         
+        plt.figure()
+        plt.plot(t_eval, actual_solutions, label="actual solution")
+        plt.plot(t_eval, computed_solutions, label="computed solution")
+        #for (x, y) in res:
+        #    plt.axvline(x=x)
+        plt.title(f"solutions for tol={tol}")
+        plt.legend()
+        plt.show()
+
+
+        error = [abs(actual_solution - computed_solution) for actual_solution, computed_solution in zip(actual_solutions, computed_solutions)]
+        plt.figure()
+        plt.plot(t_eval, error)
+        plt.title(f"error for tol={tol}")
+        plt.show()
+
+        # shape of errors - graphs
+        plt.figure()
+        for (x_i, x_i_plus_1, hb) in error_samplings:
+            num_points = 100
+            pts_to_sample = create_t_eval(x_i, x_i_plus_1, num_points)
+            errors = []
+            for i, pt in enumerate(pts_to_sample):
+                y = solution([pt])[0]
+                hb_eval = hb.eval(pt)
+                error = abs(hb_eval - y)
+                errors.append( error )
+
+                # print the error at the extremities
+                if i == 0:
+                    interpolation_error = hb_eval - hb.y_i
+                    print("error=", error, "interpolation_error=", interpolation_error)
+
+                if i == len(pts_to_sample) - 1:
+                    interpolation_error = hb_eval - hb.y_i_plus_1
+                    print("error=", error, "interpolation_error=", interpolation_error)
+
+            maximum_error = max(errors)
+            scaled_errors = [error / (maximum_error) for error in errors]
+
+            # str_x_i = "{:.3f}".format(x_i)
+            # str_x_i_plus_1 = "{:.3f}".format(x_i_plus_1)
+            x_axis = [i/(num_points - 1) for i in range(num_points)]
+            plt.plot(x_axis, scaled_errors, label=f"x_{str(x_i)}_{str(x_i_plus_1)}")
+        plt.title("plot of shape of errors")
+        plt.xlabel("step scaled between 0 and 1")
+        plt.ylabel('scaled error')
+        # plt.legend()
+        plt.show()
+
+
+        # actual_f_evals = [model(x, solution([x])[0] )[0] for x in t_eval]
+        # hb_prime_evals = [first_deriv(x) for x in t_eval]
+        # plt.figure()
+        # plt.plot(t_eval, actual_f_evals)
+        # plt.plot(t_eval, hb_prime_evals)
+        # plt.title(f"first derivative for tol={tol}")
+        # plt.show()
+
+        # defects = [abs(actual_f_eval - hb_prime_eval) for (actual_f_eval, hb_prime_eval) in zip(actual_f_evals, hb_prime_evals)]
+        # plt.figure()
+        # plt.plot(t_eval, defects)
+        # plt.title(f"global defect for tol={tol}")
+        # plt.show()
+        
+        # # defect graphs
+        # # we only pick 10 of the derivs
+        # pick_every = (len(derivs) // 5) + 1
+        # # print("pick every", pick_every)
+        # plotted_derivs = []
+        # for i in range(0, len(derivs), pick_every):
+        #     plotted_derivs.append(
+        #         derivs[i]
+        #     )
+
+        # plt.figure()
+        # for (x_i_minus_1, x_i_plus_1, hb) in plotted_derivs:
+        #     num_points = 100
+        #     pts_to_sample = create_t_eval(x_i_minus_1, x_i_plus_1, num_points)
+        #     defects = []
+        #     for pt in pts_to_sample:
+        #         y = solution([pt])[0]
+        #         f_eval  = model(pt, y)[0]
+        #         hb_prime_eval = hb.prime(pt)
+        #         defects.append( abs(hb_prime_eval - f_eval) )
+        #     maximum_defect = max(defects)
+        #     minimum_defect = min(defects)
+        #     plot_vals = [(defect - minimum_defect) / (maximum_defect - minimum_defect) for defect in defects]
+        #     #plt.plot(xs, defects, label=f"x_{str(x_i_minus_1)}_{str(x_i_plus_1)}")
+
+        #     x_axis = [i/(num_points - 1) for i in range(num_points)]
+        #     str_x_i_minus_1 = "{:.3f}".format(x_i_minus_1)
+        #     str_x_i_plus_1 = "{:.3f}".format(x_i_plus_1)
+        #     plt.plot(x_axis, plot_vals, label=f"x_{str_x_i_minus_1}_{str_x_i_plus_1}")
+        # plt.title("plot of defects")
+        # plt.xlabel("step scaled between 0 and 2")
+        # plt.ylabel('defect')
+        # plt.legend()
+        # plt.show()
+
+# experiment(model3, solution3, y0_3, t_span_3)
+experiment(model2, solution2, y0_2, t_span_2)
---
+++ defect control/error control/rk6_with_hb6_vs_hb8_errorSampling/rk6.py
@@ -0,0 +1,334 @@
+from math import sqrt
+from HB8_second_scheme import HB8, ContinuousSolution
+from HB6 import HB6
+
+
+# http://people.math.sfu.ca/~jverner/RKV65.IIIXb.Efficient.00000144617.081204.RATOnWeb
+A = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0.06, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0.019239962962962962, 0.07669337037037037, 0, 0, 0, 0, 0, 0, 0], 
+    [0.035975, 0, 0.107925, 0, 0, 0, 0, 0, 0], 
+    [1.3186834152331484, 0, -5.042058063628562, 4.220674648395414, 0, 0, 0, 0, 0], 
+    [-41.872591664327516, 0, 159.4325621631375, -122.11921356501003, 5.531743066200054, 0, 0, 0, 0], 
+    [-54.430156935316504, 0, 207.06725136501848, -158.61081378459, 6.991816585950242, -0.018597231062203234, 0, 0, 0], 
+    [-54.66374178728198, 0, 207.95280625538936, -159.2889574744995, 7.018743740796944, -0.018338785905045722, -0.0005119484997882099, 0, 0], 
+    [0.03438957868357036, 0, 0, 0.2582624555633503, 0.4209371189673537, 4.40539646966931, -176.48311902429865, 172.36413340141507, 0]
+]
+C = [0, 0.06, 0.09593333333333333, 0.1439, 0.4973, 0.9725, 0.9995, 1, 1]
+B = [0.03438957868357036, 0, 0, 0.2582624555633503, 0.4209371189673537, 4.40539646966931, -176.48311902429865, 172.36413340141507, 0]
+B_HAT = [0.04301298296577121, 0, 0, 0.23882842561019763, 0.4493871915553917, 2.2956854086040193, -73.02457612433467, 70.96432878226597, 0.03333333333333333]
+n_stages = 9
+
+# ===================================================================================================================================
+# taking one rk step with a Runge Kutta pair
+
+def sigma_prod(arr1, arr2, start, end):
+    res = 0
+    for i in range(start, end):
+        res += (  arr1[i] * arr2[i]  )
+    return res
+
+def one_step(func, xn, yn, f_start, h):
+    # theory:
+        # y_n_plus_1 = y_n + h * sigma( b[i] * k[i] ) # where i goes from 1 to n_stages
+        # k[i] = f(tn + c[i] * h, yn + h * sigma( A[i][j]) * k[j] ) where i is the current stage, j goes from 0 to current_stage-1
+    k = [0] * n_stages
+    k[0] = f_start     # we assume it is precomputed => k[0] = f(xn, yn) and that k[-1] = f(xn_plus_1, yn_plus_1)
+
+    for i in range(1, n_stages):
+        k[i] = func(   
+            # BUG => I you need to multiply by h => xn + C[i], 
+            xn + C[i] * h, 
+            yn + h * sigma_prod(A[i], k, 0, i) # sigma sums the product of the two array from start up to but excluding end
+        )[0] # f returnes an array for each component, for now everytme we can f, we will just extract the first component
+
+    yn_plus_1 = yn + h * sigma_prod(B, k, 0, n_stages) # sigma sums the product of the two array from start up to but excluding end
+
+    yn_plus_1_higher_order = yn_plus_1 # yn + h * sigma_prod(B_HAT, k, 0, n_stages) 
+
+    return (k, yn_plus_1, yn_plus_1_higher_order)
+
+class Monitor:
+    def __init__(self) -> None:
+        self.different_values_alpha = set()
+        self.different_values_beta = set()
+        self.n_steps=0
+        self.n_successful_steps=0
+    def print(self):
+        print("alpha values", list(self.different_values_alpha))
+        print("beta values", list(self.different_values_beta))
+        print("n_steps", self.n_steps)
+        print("n_successful_steps", self.n_successful_steps)
+# ===============================================================================================
+# RK defect control that allows the first step be perfect
+# this allows us to do a successful first step...
+
+def rk_error_control_perfect_first_step(fun, t_span, y0, tol, solution):
+    xn, xend = t_span
+    yn = y0
+    f_start = fun(xn, yn)[0] # each time we call, the function 'fun', we will have to extract the first value as solve_ivp wants vectorised model functions
+    
+    res = [(xn, yn)]
+    fn_s = [f_start]
+    interps = []
+
+    # we do a perfect step for the one_step
+    h = 3e-2 # as HB8 Vs at 1e-1 to 1e-2 sqrt(tol)
+    xn = xn + h
+    yn = solution([xn])[0]
+    f_start = fun(xn, yn)[0]
+    res.append( (xn, yn) )
+    fn_s.append(f_start)
+
+    xn = xn + h
+    yn = solution([xn])[0]
+    f_start = fun(xn, yn)[0]
+    res.append( (xn, yn) )
+    fn_s.append(f_start)
+
+    monitor8 = Monitor()
+    monitor6 = Monitor()
+    while xn < xend:
+        (k, yn_plus_1, yn_plus_1_higher_order) = one_step(fun, xn, yn, f_start, h)
+
+        x_i, y_i = res[-1]
+        x_i_minus_1, y_i_minus_1 = res[-2]
+        x_i_minus_2, y_i_minus_2 = res[-3]
+        x_i_plus_1, y_i_plus_1 = x_i + h, yn_plus_1_higher_order
+
+        f_i = fn_s[-1]
+        f_i_minus_1 = fn_s[-2]
+        f_i_minus_2 = fn_s[-3]
+        f_i_plus_1 = fun(x_i_plus_1, y_i_plus_1)[0]
+
+        this_interp_hb8 = HB8(
+            x_i_minus_2, x_i_minus_1, x_i, x_i_plus_1,
+            y_i_minus_2, f_i_minus_2,
+            y_i_minus_1, f_i_minus_1,
+            y_i, f_i,
+            y_i_plus_1, f_i_plus_1,
+            monitor8
+        )
+
+        this_interp_hb6 = HB6(
+            x_i_minus_1, x_i, x_i_plus_1,
+            y_i_minus_1, f_i_minus_1,
+            y_i, f_i,
+            y_i_plus_1, f_i_plus_1,
+            monitor6
+        )
+
+        monitor8.n_steps += 1
+
+        """
+        # we test the interpolant to check if the Hermite Birkhoff conditions are met as intended
+        if (abs(this_interp.eval(x_i_minus_1) - y_i_minus_1))  > 1e-12: print("wrong y_i_minus_1", abs(this_interp.eval(x_i_minus_1) - y_i_minus_1))
+        if (abs(this_interp.eval(x_i)         - y_i))          > 1e-12: print("wrong y_i",         abs(this_interp.eval(x_i)         - y_i))
+        if (abs(this_interp.eval(x_i_plus_1)  - y_i_plus_1))   > 1e-12: print("wrong y_i_plus_1",  abs(this_interp.eval(x_i_plus_1)  - y_i_plus_1))
+
+        if (abs(this_interp.prime(x_i_minus_1) - f_i_minus_1)) > 1e-12: print("wrong f_i_minus_1", abs(this_interp.prime(x_i_minus_1) - f_i_minus_1))
+        if (abs(this_interp.prime(x_i)         - f_i))         > 1e-12: print("wrong f_i",         abs(this_interp.prime(x_i)         - f_i))
+        if (abs(this_interp.prime(x_i_plus_1)  - f_i_plus_1))  > 1e-12: print("wrong f_i_plus_1",  abs(this_interp.prime(x_i_plus_1)  - f_i_plus_1))
+        """
+
+        # error control on [x_i to x_i_plus_1]
+        h_i = x_i_plus_1 - x_i
+        x_sample_1 = x_i + 0.6 * h_i
+        error_sample1 = abs(
+            this_interp_hb6.eval(x_sample_1) - this_interp_hb8.eval(x_sample_1)
+        )
+
+        x_sample_2 = x_i + 0.8 * h_i
+        error_sample2 = abs(
+            this_interp_hb6.eval(x_sample_2) - this_interp_hb8.eval(x_sample_2)
+        )
+
+        max_error_estimate = max(error_sample1, error_sample2)
+
+        if max_error_estimate < tol:
+            # accept the step, by moving the x and the y
+            xn = x_i_plus_1
+            yn = y_i_plus_1
+            res.append( (xn, yn) )
+
+            f_start = f_i_plus_1
+            fn_s.append(f_start)
+
+            monitor8.n_successful_steps += 1
+
+            interps.append(this_interp_hb8)
+            if max_error_estimate < (tol / 10):
+                h *= 2
+        else:
+            h /= 2
+
+    print("tolerance=", tol)
+    print("Monitor8\n===================================")
+    monitor8.print()
+    print("\n\nMonitor6\n===============================")
+    monitor6.print()
+
+    print("================================\n")
+    continuous_sol = ContinuousSolution()
+    continuous_sol.extend(interps)
+    return (
+        res, 
+        continuous_sol.eval,
+        continuous_sol.prime,
+        continuous_sol.create_error_samplings()
+    )
+
+# # =================================================================================
+# # the following attempt is when the solver is to keep alpha at 1 throughout the integration
+
+# # will also have solution for the first step as a proof of concept
+def rk_error_control_static_alpha_beta(fun, t_span, y0, tol, solution):
+    xn, xend = t_span
+    yn = y0
+    f_start = fun(xn, yn)[0] 
+    
+    res = [ (xn, yn) ]
+    fn_s = [f_start]
+
+    h = 3e-2 # as HB8 Vs at 1e-1 to 1e-2 sqrt(tol)
+    xn = xn + h
+    yn = solution([ xn ])[0]
+    f_start = fun(xn, yn)[0]
+    res.append( (xn, yn) )
+    fn_s.append(f_start)
+
+    xn = xn + h
+    yn = solution([ xn ])[0]
+    f_start = fun(xn, yn)[0]
+    res.append( (xn, yn) )
+    fn_s.append(f_start)
+
+    xn = xn + h
+    yn = solution([ xn ])[0]
+    f_start = fun(xn, yn)[0]
+    res.append( (xn, yn) )
+    fn_s.append(f_start)
+
+    x_i_plus_1, y_i_plus_1   = res[-1]
+    x_i, y_i                 = res[-2]
+    x_i_minus_1, y_i_minus_1 = res[-3]
+    x_i_minus_2, y_i_minus_2 = res[-4]
+
+    f_i_plus_1  = fn_s[-1]
+    f_i         = fn_s[-2]
+    f_i_minus_1 = fn_s[-3]
+    f_i_minus_2 = fn_s[-4]
+
+    monitor8 = Monitor()
+    monitor6 = Monitor()
+
+    this_interp = HB8(
+        x_i_minus_2, x_i_minus_1, x_i, x_i_plus_1,
+        y_i_minus_2, f_i_minus_2,
+        y_i_minus_1, f_i_minus_1,
+        y_i, f_i,
+        y_i_plus_1, f_i_plus_1,
+        monitor8
+    )
+
+    continous_sol = ContinuousSolution()
+    continous_sol.append(this_interp)
+
+    while xn < xend:
+        (k, yn_plus_1, yn_plus_1_higher_order) = one_step(fun, xn, yn, f_start, h)
+
+        x_i, y_i = res[-1]
+        x_i_plus_1, y_i_plus_1 = x_i + h, yn_plus_1_higher_order
+
+        f_i = fn_s[-1]
+        f_i_plus_1 = fun(x_i_plus_1, y_i_plus_1)[0]
+
+        x_i_minus_1 = x_i - h
+        y_i_minus_1 = continous_sol.eval(x_i_minus_1)
+        f_i_minus_1 = continous_sol.prime(x_i_minus_1)
+
+        x_i_minus_2 = x_i_minus_1 - h
+        y_i_minus_2 = continous_sol.eval(x_i_minus_2)
+        f_i_minus_2 = continous_sol.prime(x_i_minus_2)
+
+        this_interp_hb8 = HB8(
+            x_i_minus_2, x_i_minus_1, x_i, x_i_plus_1,
+            y_i_minus_2, f_i_minus_2,
+            y_i_minus_1, f_i_minus_1,
+            y_i, f_i,
+            y_i_plus_1, f_i_plus_1,
+            monitor8
+        )
+
+        
+        this_interp_hb6 = HB6(
+            x_i_minus_1, x_i, x_i_plus_1,
+            y_i_minus_1, f_i_minus_1,
+            y_i, f_i,
+            y_i_plus_1, f_i_plus_1,
+            monitor6
+        )
+
+        """
+        # we test the interpolant to check if the Hermite Birkhoff conditions are met as intended
+        if (abs(this_interp.eval(x_i_minus_1) - y_i_minus_1))  > 1e-12: print("wrong y_i_minus_1", abs(this_interp.eval(x_i_minus_1) - y_i_minus_1))
+        if (abs(this_interp.eval(x_i)         - y_i))          > 1e-12: print("wrong y_i",         abs(this_interp.eval(x_i)         - y_i))
+        if (abs(this_interp.eval(x_i_plus_1)  - y_i_plus_1))   > 1e-12: print("wrong y_i_plus_1",  abs(this_interp.eval(x_i_plus_1)  - y_i_plus_1))
+
+        if (abs(this_interp.prime(x_i_minus_1) - f_i_minus_1)) > 1e-12: print("wrong f_i_minus_1", abs(this_interp.prime(x_i_minus_1) - f_i_minus_1))
+        if (abs(this_interp.prime(x_i)         - f_i))         > 1e-12: print("wrong f_i",         abs(this_interp.prime(x_i)         - f_i))
+        if (abs(this_interp.prime(x_i_plus_1)  - f_i_plus_1))  > 1e-12: print("wrong f_i_plus_1",  abs(this_interp.prime(x_i_plus_1)  - f_i_plus_1))
+        """        
+
+        # error control on [x_i to x_i_plus_1]
+        h_i = x_i_plus_1 - x_i
+        x_sample_1 = x_i + 0.4 * h_i
+        error_sample1 = abs(
+            this_interp_hb6.eval(x_sample_1) - this_interp_hb8.eval(x_sample_1)
+        )
+
+        x_sample_2 = x_i + 0.6 * h_i
+        error_sample2 = abs(
+            this_interp_hb6.eval(x_sample_2) - this_interp_hb8.eval(x_sample_2)
+        )
+
+        max_error_estimate = max(error_sample1, error_sample2)
+
+        monitor8.n_steps += 1
+
+        # print("max_defect", max_defect)
+        if max_error_estimate < tol:
+            # accept the step, by moving the x and the y
+            xn = x_i_plus_1
+            yn = y_i_plus_1
+            res.append( (xn, yn) )
+
+            f_start = f_i_plus_1
+            fn_s.append(f_start)
+
+            continous_sol.append(this_interp_hb8)
+
+            monitor8.n_successful_steps += 1
+
+            if max_error_estimate < (tol / 10):
+                h *= 2
+        else:
+            # print("tolerance not satisfied")
+            h /= 2
+
+    print("tolerance=", tol)
+    print("monitor8\n============================")
+    monitor8.print()
+    print()
+
+    print("monitor6\n============================")
+    monitor6.print()
+    print("================================\n")
+
+    return (
+        res, 
+        continous_sol.eval,
+        continous_sol.prime,
+        continous_sol.create_error_samplings()
+    )
