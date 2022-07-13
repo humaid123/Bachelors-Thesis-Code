09b56be5eb716a0dc5e9b3b36540209ff8ee10f3
humaid123 <huagowun@gmail.com>
2022-07-13 18:48:44 -0300

    crk65 and crk87 with discrete error control

---
+++ defect control/error control/discrete_crk8(7)/test.py
@@ -0,0 +1,169 @@
+from crk87 import rk_error_control
+from math import sqrt, exp, cos, sin
+import matplotlib.pyplot as plt
+
+def create_t_eval(start, end, num_points = 100):
+    res = [start]
+    h = (end - start)/(num_points - 1)
+
+    for _ in range(num_points - 1):
+        res.append(
+            res[-1] + h
+        )
+    return res
+
+def plot_shapes_of_error(continous_sol, solution, name):
+    minimum_step_size = 1e-5
+    errors_small_steps = []
+    plt.figure()
+    for interp in continous_sol.interps:
+        num_points = 100
+        pts_to_sample = create_t_eval(interp.x_i, interp.x_i_plus_1, num_points)
+        errors = []
+        for pt in pts_to_sample:
+            y = solution([pt])[0]
+            interp_eval = interp.eval(pt)
+            errors.append( abs(interp_eval - y) )
+        maximum_error = max(errors)
+        plot_vals = [ error/ maximum_error for error in errors]
+        #plt.plot(xs, defects, label=f"x_{str(x_i_minus_1)}_{str(x_i_plus_1)}")
+        x_axis = [i/(num_points - 1) for i in range(num_points)]
+
+        if (interp.x_i_plus_1 - interp.x_i < minimum_step_size):
+            errors_small_steps.append([x_axis, plot_vals])
+            continue
+        plt.plot(x_axis, plot_vals, label=f"x_{str(interp.x_i)}_{str(interp.x_i_plus_1)}")
+    plt.title(f"{name}-plot of errors")
+    plt.xlabel("x_i to x_i_plus_1")
+    plt.ylabel('error/(max_error on x_i to x_i_plus_1)')
+    # plt.legend()
+    plt.show()
+    # ====================================== end figure of defect
+
+    # # ====================================== defects on small steps
+    # plt.figure()
+    # for [x_axis, plot_vals] in errors_small_steps:
+    #     plt.plot(x_axis, plot_vals)
+    # plt.title("plot of errors on small step sizes")
+    # plt.xlabel("x_i to x_i_plus_1")
+    # plt.ylabel('error/(max_error on x_i to x_i_plus_1)')
+    # plt.show()
+
+def plot_shapes_of_error_two_orders(continous_sol, lower_continous_sol):
+    minimum_step_size = 1e-5
+    errors_small_steps = []
+    plt.figure()
+    for interp in continous_sol.interps:
+        num_points = 100
+        pts_to_sample = create_t_eval(interp.x_i, interp.x_i_plus_1, num_points)
+        errors = []
+        for pt in pts_to_sample:
+            lower_interp_eval = lower_continous_sol.eval(pt)
+            interp_eval = interp.eval(pt)
+            errors.append( abs(interp_eval - lower_interp_eval) )
+        maximum_error = max(errors)
+        plot_vals = [ error/ maximum_error for error in errors]
+        #plt.plot(xs, defects, label=f"x_{str(x_i_minus_1)}_{str(x_i_plus_1)}")
+        x_axis = [i/(num_points - 1) for i in range(num_points)]
+
+        if (interp.x_i_plus_1 - interp.x_i < minimum_step_size):
+            errors_small_steps.append([x_axis, plot_vals])
+            continue
+        plt.plot(x_axis, plot_vals, label=f"x_{str(interp.x_i)}_{str(interp.x_i_plus_1)}")
+    plt.title(f"plot of errors between two orders")
+    plt.xlabel("x_i to x_i_plus_1")
+    plt.ylabel('error/(max_error on x_i to x_i_plus_1)')
+    # plt.legend()
+    plt.show()
+    # ====================================== end figure of defect
+
+    # # ====================================== defects on small steps
+    # plt.figure()
+    # for [x_axis, plot_vals] in errors_small_steps:
+    #     plt.plot(x_axis, plot_vals)
+    # plt.title("plot of errors on small step sizes")
+    # plt.xlabel("x_i to x_i_plus_1")
+    # plt.ylabel('error/(max_error on x_i to x_i_plus_1)')
+    # plt.show()
+
+def experiment(model, y0, t_span, solution):
+    t_eval = create_t_eval(t_span[0], t_span[1])
+    tol = 1e-6
+    (res, continous_sol, lower_continous_sol) = rk_error_control(model, t_span, y0[0], tol)
+    print("integration complete")
+
+    # ====================================== figure of rk6 vs rk6_interps vs rk45
+    # plt.figure()
+    xs = [x[0] for x in res]
+    # ys = [x[1] for x in res]
+    # plt.plot(xs, ys, label="rk6")
+
+    # plots of where the end of the steps occured to look at the interp
+    for this_x in xs:
+        plt.axvline(x=this_x) 
+
+    computed_solutions = [continous_sol.eval(x) for x in t_eval]
+    lower_computed_solutions = [lower_continous_sol.eval(x) for x in t_eval]
+    plt.plot(t_eval, computed_solutions, label="higher order")
+    plt.plot(t_eval, lower_computed_solutions, label="lower order")
+
+    actual_solutions = solution(t_eval)
+    plt.plot(t_eval, actual_solutions, label="solution")
+
+    # removed rk45 plt.title("solution vs rk45 vs rk6 vs rk6_interpolated")
+    plt.title("solution vs rk6_interpolated")
+    plt.xlabel("t")
+    plt.ylabel('y')
+    plt.legend(loc="upper right")
+    plt.show()
+    # ====================================== end figure of rk6 vs rk6_interps vs rk45
+
+    # ====================================== global error
+    plt.figure()
+    error = [abs(computed_solution - actual_solution) for (computed_solution, actual_solution) in zip(computed_solutions, actual_solutions)]
+    lower_order_error = [abs(computed_solution - actual_solution) for (computed_solution, actual_solution) in zip(lower_computed_solutions, actual_solutions)]
+    for this_x in xs:
+        plt.axvline(x=this_x) 
+    plt.plot(t_eval, error, label="higher order")
+    plt.plot(t_eval, lower_order_error, label="lower order")
+    plt.title("global error")
+    plt.xlabel("t")
+    plt.ylabel("error")
+    plt.legend(loc="upper right")
+    plt.show()
+    # ====================================== end of global error
+
+    plot_shapes_of_error(continous_sol, solution, "higher order")
+    plot_shapes_of_error(lower_continous_sol, solution, "lower order")
+    plot_shapes_of_error_two_orders(continous_sol, lower_continous_sol)
+    
+    # ========================================================= end of steps error vs interpolant error
+    for (x, y) in res:
+        print(abs(y - solution([ x ])[0]))
+
+
+t_span_1 = [0, 10]
+y0_1 = [1]
+def model1(t, y):
+    return [(-1/2) * y**3]
+def solution1(t):
+    return [1/sqrt(1+x) for x in t]
+experiment(model1, y0_1, t_span_1, solution1)
+
+t_span_3 = [0, 10]
+y0_3 = [1]
+def model3(t, y):
+    return [(1/4)*y*(1 - y/20)]
+def solution3(t):
+    return [20 / ( 1 + 19 * exp(-x/4) ) for x in t]
+experiment(model3, y0_3, t_span_3, solution3)
+
+t_span_7 = [0, 10]
+y0_7 = [1]
+def model7(t, y):
+    alpha = 0.1
+    return [ -alpha*y - exp(-alpha*t)*sin(t)]
+def solution7(t):
+    alpha = 0.1
+    return [exp(-alpha*x)*cos(x) for x in t]
+experiment(model7, y0_7, t_span_7, solution7)
